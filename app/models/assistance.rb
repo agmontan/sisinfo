cd130e65de2dd3ede4733ae037680eb7d47e7f71
agmontan <carlos05.ale@gmail.com>
2017-06-05 19:36:08 +0200

    Add files via upload

--- app/models/assistance.rb
+++ app/models/assistance.rb
@@ -9,4 +9,23 @@ class Assistance < ApplicationRecord
   	end
   	
   end
+
+  def calculate(one)
+  	if one.month ==	Time.now.month
+	  	if User.find(one.user_id).work == "Hour"
+	  		if User.find(one.user_id).position == "Jefe"
+	  		salary = one.hours_worked * 60
+	  		else
+	  		salary = one.hours_worked * 35
+
+	  		end	
+	  	else
+	  		if User.find(one.user_id).position =="Jefe"
+	  		salary = 20000
+	  		else
+	  		salary = 14000
+	  		end 
+	  	end
+	 end
+  end
 end
